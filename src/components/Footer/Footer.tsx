d891dd7fe0e09a06c5261689fc0d3f2612db22d2
SaSchiffer <sascha.schiffer@metro.digital>
2024-03-06 17:03:16 +0100

    Sascha | Footer | add sitemap items.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -9,15 +9,54 @@ export const FooterComponent: React.FC = (color) => {
     return  <div className='footer__container'>
                 <div className='upperFooter'>
                     <div class="sitemap">
-                        <div>Apprentices</div>
-                        <div>Instructors</div>
-                        <div>Projects</div>
-                        <div>Goals</div>
-                        <div>Milestones</div>
+                        <div class="Menu">
+                            Apprentices
+                            <ul>
+                                <li>1st year</li>
+                                <li>2nd year</li>
+                                <li>3rd year</li>
+                            </ul>
+                        </div>
+
+                        <div class="Menu">
+                            Instructors
+                            <ul>
+                                <li>Orga</li>
+                                <li>Technical</li>
+                                <li>Others</li>
+                            </ul>
+                        </div>
+
+                        <div class="Menu">
+                            Projects
+                            <ul>
+                                <li>CodeClub</li>
+                                <li>FrontendWars</li>
+                                <li>next Project</li>
+                            </ul>
+                        </div>
+
+                        <div class="Menu">
+                            How to...
+                            <ul>
+                                <li>Autofill IHK Berichtsheft</li>
+                                <li>...</li>
+                                <li>...</li>
+                            </ul>
+                        </div>
+
+                        <div class="Menu">
+                            Milestones
+                            <ul>
+                                <li>AP Teil 1</li>
+                                <li>AP Teil 2</li>
+                                <li>Klausuren BK Hilden</li>
+                            </ul>
+                        </div>
                     </div>
 
                     <div class="socialMedia">
-                        <h3>MDapprentices on Social Web</h3>
+                        <h4>MDapprentices on Social Web</h4>
                         <div class="icons">
                             <img class="icon" src="facebook-icon.png"/>
                             <img class="icon" src="insta-icon.png"/>
