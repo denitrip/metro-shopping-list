45a7f68b5af0af335429cecf61fd1f9efeada3c6
SaSchiffer <sascha.schiffer@metro.digital>
2024-03-07 17:39:49 +0100

    Sascha | Cards | add a JVS Logic to render progress bar.

--- src/components/ShoppingItemCard/ShoppingItemCard.tsx
+++ src/components/ShoppingItemCard/ShoppingItemCard.tsx
@@ -10,10 +10,25 @@ type Props = {
 }
 
 export const ShoppingCardComponent: React.FC<Props> = ({item}) => {
-    return <div className='shoppingCardItem'>
-        <div className="cardTitle">{item.skillname}</div>
-        <div className="cardProgress">Progress: {item.skillprogress} of 10</div>
-        <div className="progressTopic">"actual topic"</div>
-        <div className="cardNote">Notes:</div>
-    </div>
+        const filledSquares = Array(item.skillprogress).fill("filledSquare.png");
+        const emptySquares = Array(10 - item.skillprogress).fill("emptySquare.png");
+        const allSquares = filledSquares.concat(emptySquares);
+        return <div className='shoppingCardItem'>
+            <div className="cardContent">
+                <div className="cardTitle">{item.skillname}</div>
+                <div className="cardProgress">
+                    <img className="plusMinusButton" src="minusButton.png"/>
+                    {allSquares.map((icon, index) => (
+                        <img key={index} className="progressIcon" src={icon} alt="square" />
+                    ))}
+                    <img className="plusMinusButton" src="plusButton.png"/>
+                </div>
+                <div className="progressTopic">"actual topic"</div>
+                <div className="cardNote">Notes:</div>
+            </div>
+            <div className="sidebar">
+                <a href="https://github.com/denitrip/metro-shopping-list"><img className="xButton" src="xButton.png"/></a>
+
+            </div>
+        </div>
 }
